0fc5c1437d78b79f697d52fff667ccd1375faf26
Ícaro Rodrigues <96674451+PZicaro@users.noreply.github.com>
2022-11-11 02:02:36 -0300

    Update Animal.java

--- Programa/src/Animal.java
+++ Programa/src/Animal.java
@@ -1,2 +1,50 @@
+package projeto;
+
+import java.util.Date;
+
 public class Animal {
+ private String Apelido;
+ private String tipoAnimal;
+ private Date dataNascimento;
+ private String dono;
+ 
+ public Animal( String Apelido, String tipoAnimal,  Date dataNascimento,String dono) {
+	 this.Apelido=Apelido;
+	 this.tipoAnimal =tipoAnimal;
+	 this.dataNascimento =dataNascimento;
+	 this.dono = dono;
+	 
+ }
+ public String getApelido() {
+	 return Apelido;
+ }
+ public void setApelido(String novoApelido) {
+	 Apelido = novoApelido;
+ }
+ public String getTipoAnimal() {
+	 return tipoAnimal;
+ }
+ public void setTipoAnimal(String novoTipoAnimal) {
+	 tipoAnimal = novoTipoAnimal;
+ }
+ public Date getDataNascimento() {
+	 return dataNascimento;
+ }
+ public void setDataNascimento(Date novaDataNascimento) {
+	 dataNascimento = novaDataNascimento;
+	 
+ }
+ public String getDono() {
+	 return dono;
+ }
+ public void setDono(String novoDono) {
+	 dono = novoDono;
+ }
+ public void dados() {
+	 System.out.println("O apelido do cachorro é:" + Apelido );
+	 System.out.println("o tipo do animal é:" + tipoAnimal);
+	 System.out.println("a data do nascimento é:" + dataNascimento);
+	 System.out.println("O dono do animal é:" + dono);
+ }
 }
+
