be40fbd9bad6be4fbb57a63f1d7cf564bb17b5de
Mariana <moreiramarimana137@gmail.com>
2022-11-11 01:38:42 -0300

    Inserindo a primeira parte da classe Atendimento, feita por Brendo

--- Programa/src/Atendimento.java
+++ Programa/src/Atendimento.java
@@ -1,2 +1,25 @@
 public class Atendimento {
+
+    // Variáveis
+    private Servico servico;
+    private Animal animal;
+    private String senha;
+    /* falta as variaveis da data !!! */
+
+    // construtor
+    public Atendimento(Servico servico, Animal animal, String senha) {
+        this.servico = servico;
+        this.animal = animal;
+        this.senha = senha;
+    }
+
+    // get e set
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
 }
