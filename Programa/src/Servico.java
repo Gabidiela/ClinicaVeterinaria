67a9f82308381d0f6ec23561edc9498e69149930
Mariana <moreiramarimana137@gmail.com>
2022-11-11 01:32:21 -0300

    Inserindo a classe Serviço

--- Programa/src/Servico.java
+++ Programa/src/Servico.java
@@ -1,2 +1,30 @@
 public class Servico {
+    // Variáveis
+    private String nome;
+    private String tipo;
+
+    // Construtor
+    public Servico(String nome, String tipo) {
+        this.nome = nome;
+        this.tipo = tipo;
+    }
+
+    // getters e setters
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
 }
