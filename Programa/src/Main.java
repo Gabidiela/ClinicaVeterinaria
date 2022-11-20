69681cf1fbc03ea5bc5db0c08d79ed8f7d7612ca
arirocha <104226625+arirocha@users.noreply.github.com>
2022-11-19 21:42:49 -0300

    Corrigindo alguns erros

--- Programa/src/Main.java
+++ Programa/src/Main.java
@@ -15,13 +15,15 @@ public class Main {
         boolean sair = false;
 
         do {
-            System.out.println("O que deseja fazer?");
-            System.out.println("1. Cadastrar Animal");
-            System.out.println("2. Realizar Atendimento");
-            System.out.println("3. Chamar proximo Atendimento");
-            System.out.println("2. Realizar Atendimento");
-            System.out.println("9. Encerrar programa");
+            System.out.println("************** MENU **************);
+            System.out.println("* O que deseja fazer?           *");
+            System.out.println("* 1. Cadastrar Animal           *");
+            System.out.println("* 2. Realizar Atendimento       *");
+            System.out.println("* 3. Chamar proximo Atendimento *")
+            System.out.println("* 0. Encerrar programa          *");
+            System.out.println("*********************************");          
             int op = entrada.nextInt();
+            entrada.nextLine();
 
             switch (op){
                 case 1:
@@ -31,8 +33,14 @@ public class Main {
                 	
                 	System.out.print("Tipo do animal:  (canino, felino ou roedor)");
                     String tipoAnimal  = entrada.nextLine();
+                    
+                    System.out.print("Dono: ");
+                    String dono  = entrada.nextLine();
+                
                     System.out.print("Data de nascimento: (dd/MM/yyyy)");
                     String data = entrada.nextLine();
+                    entrada.nextLine();
+                
                     Animal animal;
                     Date dataNascimento = null;
                     try {
@@ -41,8 +49,6 @@ public class Main {
                 	} catch (ParseException e) {
                 		e.printStackTrace();
                 	}
-                    System.out.print("Dono: ");
-                    String dono  = entrada.nextLine();
                     
                     animal = new Animal(Apelido, tipoAnimal, dataNascimento, dono);
                     System.out.println(clinica.cadastrarAnimal(animal));
@@ -59,22 +65,22 @@ public class Main {
 
                     switch (sel){
                         case 1:
-                            clinica.CadConsulta(new Atendimento(selecionarAnimal(),"Vacinação",false));
+                            clinica.cadConsulta(new Atendimento(selecionarAnimal(),"Vacinação",false));
                             break;
                         case 2:
-                            clinica.CadConsulta(new Atendimento(selecionarAnimal(),"Castração",false));
+                            clinica.cadConsulta(new Atendimento(selecionarAnimal(),"Castração",false));
                             break;
                         case 3:
-                            clinica.CadConsulta(new Atendimento(selecionarAnimal(),"Check-up",false));
+                            clinica.cadConsulta(new Atendimento(selecionarAnimal(),"Check-up",false));
                             break;
                         case 4:
-                            clinica.CadConsulta(new Atendimento(selecionarAnimal(),"Urgência",true));
+                            clinica.cadConsulta(new Atendimento(selecionarAnimal(),"Urgência",true));
                             break;
                         case 5:
                             break;
                     }
                     break;
-                case 9:
+                case 0:
                     System.out.println("O programa será encerrado. Até logo!");
                     sair = true;
                     break;
