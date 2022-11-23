51ee59ee7ff139881b65e362fd31e3b60540132b
arirocha <104226625+arirocha@users.noreply.github.com>
2022-11-23 00:27:09 -0300

    Inserção de alguns métodos

--- Programa/src/ClinicaVet.java
+++ Programa/src/ClinicaVet.java
@@ -56,51 +56,129 @@ public class ClinicaVet {
   public Animal fichaProxAnimal() {
     return null;
   }
-/*
   public int relAnimalFila1() {
-    int qtdPrioritaria = filaPrioritaria.size() + 1;
-      return qtdPrioritaria;
+    int qtdPrioritaria = filaPrioritaria.size();
+    return qtdPrioritaria;
   }
   public int relAnimalFila2() {
-    int qtdNormal = filaNormal.size() + 1;
-      return qtdNormal;
+    int qtdNormal = filaNormal.size();
+    return qtdNormal;
   }
-  
-  public String relAnimalFaixa() {
+  public String relAnimalFaixaN() {
     Date agora = new Date();
     int crianca=0, adulto=0, idoso=0; 
-    
-    it = atendidos.iterator();
+
+    it = filaNormal.iterator();
     while (it.hasNext()){ 
       int anoAtual = agora.getYear();
-      int anoNascimento = atendidos.peek().getAnimal().getDataNascimento().getYear();
+      int anoNascimento = filaNormal.peek().getAnimal().getDataNascimento().getYear();
       int idade = anoAtual - anoNascimento; 
-  
+
       int mesAtual = agora.getMonth();
-      int mesNascimento = atendidos.peek().getAnimal().getDataNascimento().getMonth();
+      int mesNascimento = filaNormal.peek().getAnimal().getDataNascimento().getMonth();
       if (mesAtual < mesNascimento){
         idade--;
       } else {
         int diaAtual = agora.getDay();
-        int diaNascimento = atendidos.peek().getAnimal().getDataNascimento().getDay();
+        int diaNascimento = filaNormal.peek().getAnimal().getDataNascimento().getDay();
         if (mesAtual == mesNascimento && diaAtual < diaNascimento){
           idade--;
         }
       }
-      if (idade < 2){
+      if (idade <= 2){
         crianca++;
-      } else if(idade >=2 && idade <5) {
+      } else if(idade >2 && idade <=5) {
         adulto++;
       } else {
         idoso++;
       }
+      it.next();
     }
-    return String.format("a) até 2 anos de idade %d%n", crianca);
-           String.format("b) entre 2 e 5 anos de idade %d%n", adulto); 
-           String.format("c) mais de 5 anos de idade %d%n", idoso);
+    return String.format("---------- Fila Normal ----------%n") +
+        String.format("a) até 2 anos de idade %d%n", crianca) +
+        String.format("b) entre 2 e 5 anos de idade %d%n", adulto) + 
+        String.format("c) mais de 5 anos de idade %d%n", idoso) +
+        String.format("-------------------------------");
+        
   }
-   */
+  public String relAnimalFaixaP() {
+    Date agora = new Date();
+    int crianca=0, adulto=0, idoso=0; 
 
+    it = filaPrioritaria.iterator();
+    while (it.hasNext()){ 
+      int anoAtual = agora.getYear();
+      int anoNascimento = filaPrioritaria.peek().getAnimal().getDataNascimento().getYear();
+      int idade = anoAtual - anoNascimento; 
+
+      int mesAtual = agora.getMonth();
+      int mesNascimento = filaPrioritaria.peek().getAnimal().getDataNascimento().getMonth();
+      if (mesAtual < mesNascimento){
+        idade--;
+      } else {
+        int diaAtual = agora.getDay();
+        int diaNascimento = filaPrioritaria.peek().getAnimal().getDataNascimento().getDay();
+        if (mesAtual == mesNascimento && diaAtual < diaNascimento){
+          idade--;
+        }
+      }
+      if (idade <= 2){
+        crianca++;
+      } else if(idade >2 && idade <=5) {
+        adulto++;
+      } else {
+        idoso++;
+      }
+      it.next();
+    }
+    return String.format("------ Fila Prioritária ------%n") +
+      String.format("a) até 2 anos de idade %d%n", crianca) +
+        String.format("b) entre 2 e 5 anos de idade %d%n", adulto) + 
+        String.format("c) mais de 5 anos de idade %d%n", idoso) +
+        String.format("-------------------------------");
+  }
+  public String relAnimalEsperaN() {
+    int vacinacaoN=0, castracaoN=0, checkUpN=0;
+    it = filaNormal.iterator();
+
+    while (it.hasNext()){
+      if(filaNormal.peek().getNomeServico().equalsIgnoreCase("Vacinação")){
+        vacinacaoN++;
+      } else if(filaNormal.peek().getNomeServico().equalsIgnoreCase("Castração")){
+        castracaoN++;
+      }else if(filaNormal.peek().getNomeServico().equalsIgnoreCase("Check-up")){
+        checkUpN++;
+      }
+      it.next();
+    }
+
+      return String.format("----- Fila Normal -----%n") +
+             String.format("a) Vacinação: %d%n", vacinacaoN) +
+             String.format("b) Castração: %d%n", castracaoN) + 
+             String.format("c) Check-Up: %d%n", checkUpN) +
+             String.format("-----------------------");
+  }
+   public String relAnimalEsperaP() {
+      int vacinacaoP=0, castracaoP=0, checkUpP=0;
+      it = filaPrioritaria.iterator();
+
+      while(it.hasNext()){
+        if(filaPrioritaria.peek().getNomeServico().equalsIgnoreCase("Vacinação")){
+          vacinacaoP++;
+        } else if(filaPrioritaria.peek().getNomeServico().equalsIgnoreCase("Castração")){
+          castracaoP++;
+        }else if(filaPrioritaria.peek().getNomeServico().equalsIgnoreCase("Check-up")){
+          checkUpP++;
+        }
+        it.next();
+      }
+      return String.format("--- Fila Prioritária ---%n") +
+             String.format("a) Vacinação: %d%n", vacinacaoP) +
+             String.format("b) Castração: %d%n", castracaoP) + 
+             String.format("c) Check-Up: %d%n", checkUpP) +
+             String.format("------------------------");
+    }
+  
   public void relAnimalEspera() {
     
   }
